f29a572bb9236d345fd0994055236ae371c74448
unknown <binhduyson@gmail.com>
2021-01-30 20:31:49 +0700

    Creat Router

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import { RouterModule, Routes } from '@angular/router';
+import { mainRoutes } from './main.routes';
+import { UserModule } from './user/user.module';
+
 
 
 
 @NgModule({
   declarations: [MainComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    UserModule,
+    RouterModule.forChild(mainRoutes)
   ]
 })
 export class MainModule { }
